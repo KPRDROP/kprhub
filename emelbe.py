4671d3ffd0a2e6f8b7f260283d465aec96b81e5a
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-03-07 21:07:24 -0400

    Update emelbe.py

--- emelbe.py
+++ emelbe.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin, quote
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
 
-from utils import Cache, Time, get_logger, leagues, network
+# from utils import Cache, Time, get_logger, leagues, network
 
 log = get_logger(__name__)
 
